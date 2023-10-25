460fd31f3da883327f5cedd66e711c5c6e591f77
Dmytro Miroshnychenko <miroshnychenkodmitriy@gmail.com>
2023-10-25 22:14:51 +0300

    added method once in fails index.mjs to folder 06

--- 06-events/index.mjs
+++ 06-events/index.mjs
@@ -8,3 +8,10 @@ myEmmiter.on('timeout', (secondsQty) => {
 
 setTimeout(() => myEmmiter.emit('timeout', 1), 1000);
 setTimeout(() => myEmmiter.emit('timeout', 2), 2000);
+
+myEmmiter.once('singleEvent', () => {
+  console.log('Single events occured');
+});
+//Listener function will be called only once!
+setTimeout(() => myEmmiter.emit('singleEvent'), 500);
+setTimeout(() => myEmmiter.emit('singleEvent'), 1500);
