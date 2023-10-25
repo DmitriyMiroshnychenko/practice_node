bf56d93ac8c8a41283e12d9d847ee7ad43da4104
Dmytro Miroshnychenko <miroshnychenkodmitriy@gmail.com>
2023-10-25 19:46:51 +0300

    added new folder to ES6

--- 04-es6-modiles/index.mjs
+++ 04-es6-modiles/index.mjs
@@ -1,6 +1,10 @@
 import { season, temperature } from './named-exports.mjs';
 import { isRaning, humidity } from './inline-exports.mjs';
 import getDataFromServer from './default-exports.mjs';
+import DEFAULT_SERVER, {
+  USERNAME as MY_USERNAME,
+  PASSWORD as MY_PASSWORD,
+} from './mixed-exports.mjs';
 
 console.log(season);
 console.log(temperature);
@@ -8,6 +12,9 @@ console.log(temperature);
 console.log(isRaning);
 console.log(humidity);
 
-getDataFromServer('https://jsonplaceholder.typicode.com/posts/')
+getDataFromServer('https://jsonplaceholder.typicode.com/posts/1')
   .then((post) => console.log(post))
   .catch((err) => console.error(err));
+
+console.log(DEFAULT_SERVER);
+console.log(MY_USERNAME, MY_PASSWORD);
