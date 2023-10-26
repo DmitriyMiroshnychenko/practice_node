95fd1a829420e11ebeceb01b60a2bb60078bd115
Dmytro Miroshnychenko <miroshnychenkodmitriy@gmail.com>
2023-10-26 07:50:29 +0300

    added listener mute

--- 06-events/index.mjs
+++ 06-events/index.mjs
@@ -2,9 +2,11 @@ import { EventEmitter } from 'events';
 
 const myEmmiter = new EventEmitter();
 
-myEmmiter.on('timeout', (secondsQty) => {
+const timeoutListenerFn = (secondsQty) => {
   console.log(`Temeout event in ${secondsQty} seconds!`);
-});
+};
+
+myEmmiter.on('timeout', timeoutListenerFn);
 
 setTimeout(() => myEmmiter.emit('timeout', 1), 1000);
 setTimeout(() => myEmmiter.emit('timeout', 2), 2000);
@@ -15,3 +17,7 @@ myEmmiter.once('singleEvent', () => {
 //Listener function will be called only once!
 setTimeout(() => myEmmiter.emit('singleEvent'), 500);
 setTimeout(() => myEmmiter.emit('singleEvent'), 1500);
+
+//Remove listener from the timeout event!
+setTimeout(() => myEmmiter.off('timeout', timeoutListenerFn), 3000);
+setTimeout(() => myEmmiter.emit('timeout', 4), 4000);
