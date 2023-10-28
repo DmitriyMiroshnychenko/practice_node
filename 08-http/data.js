2c83b8e5f53c2ec8c76ff19efae402840526e995
Dmytro Miroshnychenko <miroshnychenkodmitriy@gmail.com>
2023-10-28 22:48:59 +0300

    added code separation in files in folder 08

---
+++ 08-http/data.js
@@ -0,0 +1,7 @@
+const comments = [
+  { id: '100', text: 'This first comment', author: 'Dmytro' },
+  { id: '254', text: 'Second first comment', author: 'Alice' },
+  { id: '398', text: 'Last first comment', author: 'Bob' },
+];
+
+module.exports = comments;
--- 08-http/index.js
+++ 08-http/index.js
@@ -1,45 +1,18 @@
 const http = require('http');
+const { getHTML, getComments, getText, handleNotFound } = require('./handlers');
 
-const PORT = 5551;
-
-const comments = [
-  { id: '100', text: 'This first comment', author: 'Dmytro' },
-  { id: '254', text: 'Second first comment', author: 'Alice' },
-  { id: '398', text: 'Last first comment', author: 'Bob' },
-];
+const PORT = 5000;
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/html') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html><body><div>');
-    res.write('<h1>Greetings from the HTTP server!</h1>');
-    res.write('</div></body></html>');
-    return res.end();
+  if (req.method === 'GET' && req.url === '/html') {
+    return getHTML(req, res);
   }
-  if (req.url === '/text') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    return res.end('This is plain text');
+  if (req.method === 'GET' && req.url === '/text') {
+    return getText(req, res);
   }
-  if (req.url === '/json') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    return res.end(JSON.stringify(comments));
+  if (req.method === 'GET' && req.url === '/comments') {
+    return getComments(req, res);
   }
-
-  // Обработка несуществующих URL
-  res.statusCode = 404;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Page not found!</h1>');
+  handleNotFound(req, res);
 });
-
-// Обработка ошибок
-server.on('error', (error) => {
-  console.error('Server error:', error);
-});
-
-// Запуск сервера
-server.listen(PORT, () => {
-  console.log(`Server was launched on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
