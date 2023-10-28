40166a6746c57948be89796813145027234b7205
Dmytro Miroshnychenko <miroshnychenkodmitriy@gmail.com>
2023-10-28 23:39:31 +0300

    added a check for types of requests from the client

--- 08-http/handlers.js
+++ 08-http/handlers.js
@@ -6,35 +6,47 @@ function getHTML(req, res) {
   res.write('<html><body><div>');
   res.write('<h1>Greetings from the HTTP server!</h1>');
   res.write('</div></body></html>');
-  return res.end();
+  res.end();
 }
 
 function getText(req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  return res.end('This is plain text');
+  res.end('This is plain text');
 }
 
 function getComments(req, res) {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  return res.end(JSON.stringify(comments));
+  res.end(JSON.stringify(comments));
 }
 
 function postComment(req, res) {
-  let commentJSON = '';
-  req.on('data', (chunk) => (commentJSON += chunk));
-  req.on('end', () => {
-    comments.push(JSON.parse(commentJSON));
-    res.statusCode = 200;
-    res.end('Comment data was received');
-  });
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.headers['content-type'] === 'application/json') {
+    let commentJSON = '';
+    req.on('data', (chunk) => (commentJSON += chunk));
+    req.on('end', () => {
+      try {
+        comments.push(JSON.parse(commentJSON));
+        res.statusCode = 200;
+        res.end('Comment data was received');
+      } catch (error) {
+        res.statusCode = 400;
+        res.end('Invalid JSON');
+      }
+    });
+  } else {
+    res.statusCode = 400;
+    res.end;
+    ('Data must be in the JSON format');
+  }
 }
 
 function handleNotFound(req, res) {
   res.statusCode = 404;
   res.setHeader('Content-Type', 'text/html');
-  return res.end('<h1>Page not found!</h1>');
+  res.end('<h1>Page not found!</h1>');
 }
 
 module.exports = {
