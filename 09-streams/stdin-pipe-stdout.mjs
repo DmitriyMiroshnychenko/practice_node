4cb5f39c7002964994b363def6323fb47e76cf87
Dmytro Miroshnychenko <miroshnychenkodmitriy@gmail.com>
2023-10-29 20:45:51 +0200

    solution to the problem with transformation flow

--- 09-streams/stdin-pipe-stdout.mjs
+++ 09-streams/stdin-pipe-stdout.mjs
@@ -7,7 +7,16 @@ const upperCaseStream = new Transform({
     cb(null, upperCased);
   },
 });
-process.stdin.pipe(upperCaseStream).pipe(process.stdout);
+//Task with transform
+const reverseStream = new Transform({
+  transform(chunk, encoding, cb) {
+    const arrayOfChars = chunk.toString().split('');
+    const lastChar = arrayOfChars.pop();
+    const reversed = arrayOfChars.reverse().concat(lastChar).join('');
+    cb(null, reversed);
+  },
+});
+process.stdin.pipe(upperCaseStream).pipe(reverseStream).pipe(process.stdout);
 
 // //Pipe to file
 // const filePath = './files/stdin-dump.txt';
