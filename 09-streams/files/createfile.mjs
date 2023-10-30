1b454310fa03bdf04fe530d057503d075f2dd2cc
Dmytro Miroshnychenko <miroshnychenkodmitriy@gmail.com>
2023-10-30 22:20:26 +0200

    writing data to a file in a stream

--- 09-streams/files/createfile.mjs
+++ 09-streams/files/createfile.mjs
@@ -1,4 +1,7 @@
 //How to run program: node createfile.mjs <fileName> <linesQty>
+import fs from 'fs';
+import path from 'path';
+
 if (!process.argv[2] || !process.argv[3]) {
   console.log('Filename and lines qty must be supplied as arguments');
   process.exit(0);
@@ -10,3 +13,14 @@ if (isNaN(linesQty)) {
   console.log('Lines qty must be a number');
   process.exit(0);
 }
+
+const writeStream = fs.createWriteStream(path.join('./files', fileName));
+for (let i = 1; i <= linesQty; i++) {
+  writeStream.write(
+    `This is a line number ${i} in the automatically generated file\n`
+  );
+}
+
+writeStream.end(() => {
+  console.log(`Automatically generated file ${fileName} was created!`);
+});
