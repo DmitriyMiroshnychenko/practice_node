b848485c2faef4e37c767698750cc8f660fcb8cc
Dmytro Miroshnychenko <miroshnychenkodmitriy@gmail.com>
2023-10-30 21:34:15 +0200

    added arguments in progrma Node.js

---
+++ 09-streams/files/createfile.mjs
@@ -0,0 +1,12 @@
+//How to run program: node createfile.mjs <fileName> <linesQty>
+if (!process.argv[2] || !process.argv[3]) {
+  console.log('Filename and lines qty must be supplied as arguments');
+  process.exit(0);
+}
+// console.log('Continue...');
+const fileName = process.argv[2];
+const linesQty = parseInt(process.argv[3]);
+if (isNaN(linesQty)) {
+  console.log('Lines qty must be a number');
+  process.exit(0);
+}
