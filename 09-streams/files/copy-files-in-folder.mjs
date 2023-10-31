743c7d97b85ee29cbd5c242f7c670ad20fd2055e
Dmytro Miroshnychenko <miroshnychenkodmitriy@gmail.com>
2023-10-31 22:33:13 +0200

    addede stream move files from folder to folder

--- 09-streams/files/copy-files-in-folder.mjs
+++ 09-streams/files/copy-files-in-folder.mjs
@@ -1,16 +1,39 @@
 import fs from 'fs';
+import path from 'path';
 
 const sourceDir = './files';
 const destinationDir = './copied-files';
 
 if (!fs.existsSync(sourceDir)) {
-  console.warn(`Sourse dir ${sourceDir} doesnt exist!`);
+  console.warn(`Source dir ${sourceDir} doesnt exist!`);
   console.log('Exiting...');
   process.exit(0);
 }
 
 if (fs.existsSync(destinationDir)) {
-  fs.rmdirSync(destinationDir);
+  fs.rmSync(destinationDir);
   console.log('Destination dir removed');
 }
 fs.mkdirSync(destinationDir);
+
+fs.readdir(sourceDir, (err, fileNames) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  // console.log(fileNames);
+  fileNames.forEach((fileName, index) => {
+    const sourceFilePath = path.join(sourceDir, fileName);
+    const destinationFilePath = path.join(
+      destinationDir,
+      `${index}. ${fileName}`
+    );
+    const readFileStream = fs.createReadStream(sourceFilePath);
+    const writeFileStream = fs.createWriteStream(destinationFilePath);
+
+    readFileStream.pipe(writeFileStream);
+    writeFileStream.on('finish', () => {
+      console.log(`File ${fileName} was copied`);
+    });
+  });
+});
