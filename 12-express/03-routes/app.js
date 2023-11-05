2fc0f9b705b3521524b63789ee25bee0c4c910a0
Dmytro Miroshnychenko <miroshnychenkodmitriy@gmail.com>
2023-11-05 12:40:56 +0200

    added routes in new folder

--- 12-express/03-routes/app.js
+++ 12-express/03-routes/app.js
@@ -5,22 +5,39 @@ const app = express();
 const getRootHandler = (req, res) => {
   res.send('Get root route');
 };
-
+//Coments
 const getCommentsHandler = (req, res) => {
   res.send('Get comments route');
 };
-
-const getCommentHandler = (req, res) => {
+const getSingleCommentHandler = (req, res) => {
   res.send(`Get comment route. CommentId ${req.params.commentId}`);
 };
-
 const postCommentsHandler = (req, res) => {
   res.send('Post comments route');
 };
+const deleteCommentHandler = (req, res) => {
+  res.send(`Delete comment route. CommentId ${req.params.commentId}`);
+};
+
+//Users
+const getUsersHandler = (req, res) => {
+  res.send('Get users route');
+};
+const getSingleUserHandler = (req, res) => {
+  res.send(`Get user route. UserId ${req.params.userId}`);
+};
+const postUsersHandler = (req, res) => {
+  res.send('Post users route');
+};
 
 app.get('/', getRootHandler);
-app.get('/comments', getCommentsHandler);
-app.post('/comments', postCommentsHandler);
-app.get('/comments/:commentId', getCommentHandler);
+
+// app.route('/comments').get(getCommentsHandler).post(postCommentsHandler);
+
+//Comments
+
+app.get('/users', getUsersHandler);
+app.post('/users', postUsersHandler);
+app.get('/users/:uaerId', getSingleUserHandler);
 
 app.listen(5000, () => console.log('Server was started on port 5000'));
