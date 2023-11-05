7412b5d3f9e34dcc801d4cfe45b07540a47a0561
Dmytro Miroshnychenko <miroshnychenkodmitriy@gmail.com>
2023-11-05 14:48:10 +0200

    changed failes

--- 12-express/03-routes/app.js
+++ 12-express/03-routes/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const commentsRouter = require('./routes/comments');
+const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -7,26 +8,10 @@ const getRootHandler = (req, res) => {
   res.send('Get root route');
 };
 
-//Users
-const getUsersHandler = (req, res) => {
-  res.send('Get users route');
-};
-const getSingleUserHandler = (req, res) => {
-  res.send(`Get user route. UserId ${req.params.userId}`);
-};
-const postUsersHandler = (req, res) => {
-  res.send('Post users route');
-};
-
 app.get('/', getRootHandler);
 app.use('/comments', commentsRouter);
+app.use('/users', usersRouter);
 
 // app.route('/comments').get(getCommentsHandler).post(postCommentsHandler);
-
-//Comments
-
-app.get('/users', getUsersHandler);
-app.post('/users', postUsersHandler);
-app.get('/users/:uaerId', getSingleUserHandler);
 
 app.listen(5000, () => console.log('Server was started on port 5000'));
