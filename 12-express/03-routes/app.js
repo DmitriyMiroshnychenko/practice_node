5f8ef5cae713e599c843adf92d02d34f40125aee
Dmytro Miroshnychenko <miroshnychenkodmitriy@gmail.com>
2023-11-05 15:21:58 +0200

    changed file

--- 12-express/03-routes/app.js
+++ 12-express/03-routes/app.js
@@ -1,17 +1,8 @@
 const express = require('express');
-const commentsRouter = require('./routes/comments');
-const usersRouter = require('./routes/users');
+const router = require('./routes');
 
 const app = express();
 
-const getRootHandler = (req, res) => {
-  res.send('Get root route');
-};
-
-app.get('/', getRootHandler);
-app.use('/comments', commentsRouter);
-app.use('/users', usersRouter);
-
-// app.route('/comments').get(getCommentsHandler).post(postCommentsHandler);
+app.use(router);
 
 app.listen(5000, () => console.log('Server was started on port 5000'));
