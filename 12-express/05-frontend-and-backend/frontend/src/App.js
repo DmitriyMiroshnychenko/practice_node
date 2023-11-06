aa432b6180b5919ebad9632ffe01da793d260d4c
Dmytro Miroshnychenko <miroshnychenkodmitriy@gmail.com>
2023-11-06 09:48:56 +0200

    added new markup in react folder

--- 12-express/05-frontend-and-backend/frontend/src/App.js
+++ 12-express/05-frontend-and-backend/frontend/src/App.js
@@ -1,14 +1,27 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [person, setPerson] = useState();
   useEffect(() => {
     fetch('http://127.0.0.1:5000')
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => {
+        console.log(data);
+        setPerson(data);
+      })
       .catch((err) => console.log(err));
   }, []);
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {person && (
+        <>
+          <h1>{person.name}</h1>
+          <h2>{person.isDriver ? 'driver' : 'Student'}</h2>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
